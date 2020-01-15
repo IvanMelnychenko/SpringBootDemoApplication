44d5df0c91c0c7c06fc67f889d9b3f3e8562b8a4
Ivan Melnychenko <liftiwan1996@gmail.com>
2020-01-15 11:27:50 +0200

    third commit

--- src/main/java/com/gmail/liftiwan1996/springbootdemo/controller/UserController.java
+++ src/main/java/com/gmail/liftiwan1996/springbootdemo/controller/UserController.java
@@ -4,18 +4,19 @@ import com.gmail.liftiwan1996.springbootdemo.model.User;
 import com.gmail.liftiwan1996.springbootdemo.service.UserServiceImpl;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
 
-  @Autowired
+
   private final UserServiceImpl userService;
 
+  @Autowired
   public UserController(UserServiceImpl userService) {
     this.userService = userService;
   }
@@ -26,20 +27,16 @@ public class UserController {
     return users;
   }
 
-  @GetMapping("/user-create")
-  public String createUserForm(User user) {
-    return "user create";
-  }
-
   @PostMapping("/user-create")
-  public String createUser(User user) {
+  public void createUserForm(@RequestParam String firstName, @RequestParam String lastName ) {
+    User user = new User(firstName,lastName);
     userService.saveUser(user);
-    return "redirect:/users";
   }
 
   @GetMapping("/user-delete/{id}")
-  public String deleteUser(@PathVariable("id") Long id) {
+  public List deleteUser(@PathVariable("id") Long id) {
     userService.deleteById(id);
-    return "redirect:/users";
+    List<User> users = userService.findAll();
+    return users;
   }
 }
